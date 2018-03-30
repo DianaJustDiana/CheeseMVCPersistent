f25fc4e6b38a3488d857f4f2076de7ec04ac4f25
Diana Davis <dianadryerdavis@gmail.com>
2018-03-30 12:43:20 -0400

    Added CategoryController with context.

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View();
+        }
+
+
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+    }
+}
