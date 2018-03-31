8a9ce03622c147228e05cde7e9629ba7a5f30ef4
Diana Davis <dianadryerdavis@gmail.com>
2018-03-31 11:15:36 -0400

    Added MenuController action Index.

---
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            List<Menu> menus = context.Menus.ToList();
+
+            ViewBag.title = "All the menus";
+            
+            return View(menus);
+        }
+        
+        
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+    }
+}
