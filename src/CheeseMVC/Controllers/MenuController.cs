7fa296522109407114110fbe0815a6a35a31b1dd
Diana Davis <dianadryerdavis@gmail.com>
2018-03-31 12:23:11 -0400

    Tweaked the Add Menu form and MenuController/Add post method.

--- src/CheeseMVC/Controllers/MenuController.cs
+++ src/CheeseMVC/Controllers/MenuController.cs
@@ -21,10 +21,31 @@ namespace CheeseMVC.Controllers
         //get
         public IActionResult Add()
         {
-            AddMenuViewModel addMenuViewModel = new AddMenuViewModel(context.Menus.ToList());
+            AddMenuViewModel addMenuViewModel = new AddMenuViewModel();
+            return View(addMenuViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddMenuViewModel addMenuViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the new menu to my existing menus
+                Menu newMenu = new Menu
+                {
+                    Name = addMenuViewModel.Name,
+                };
+
+                context.Menus.Add(newMenu);
+                context.SaveChanges();
+
+                return Redirect("/Menu/ViewMenu/" + newMenu.ID);
+            }
+            // if !Model.State.IsValid it skips down to here
             return View(addMenuViewModel);
         }
        
+
         private readonly CheeseDbContext context;
 
         public MenuController(CheeseDbContext dbContext)
